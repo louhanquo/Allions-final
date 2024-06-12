dc491c00bfba33a71a60bc7b77c0de861fd83e4f
louhanquo <alfalionellofann@gmail.com>
2024-06-13 01:46:31 +0700

    Update AppHeader.jsx

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -13,7 +13,7 @@ const appNavBar = ()=>{
     return(
         <nav id="nav" className="navbar navbar-expand-lg navbar-light sticky-top bg-white shadow-sm">
             <div className="container">
-                <a className="navbar-brand" href="/">College Finder</a>
+                <a className="navbar-brand" href="/">Allions.edu</a>
 
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
