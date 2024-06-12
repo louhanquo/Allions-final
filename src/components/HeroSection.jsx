bc5165a043657aa92e1afd1aa48e91c028b1d21e
louhanquo <alfalionellofann@gmail.com>
2024-06-13 01:46:01 +0700

    Update HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,5 +1,3 @@
-const sourceCodeUrl = 'https://github.com/MoQuayson/college-finder/archive/refs/heads/master.zip';
-
 export default function HeroSection(){
     return(
         <section id="heroSection" className="section">
@@ -7,8 +5,9 @@ export default function HeroSection(){
                 <div className="text-center text-wrap">
                     <h1 className="hero-title">Looking for a college to enroll?</h1>
                     <p className="hero-subtitle">Try the college finder app to view colleges of any country of choice</p>
+                    <a className="btn btn-primary shadow me-2" href="https://alions-project-lionel.vercel.app/">Allions properties</a>
                     <a className="btn btn-primary shadow me-2" href="#collegeFinderApp">Try Finder App</a>
-                    <a className="btn btn-outline-info shadow" href={sourceCodeUrl}>Get Source Code</a>
+                    <a className="btn btn-primary shadow me-2" href="https://travel-advisor-jsm.netlify.app/">Allions trip advisor</a>
                 </div>
             </div>
         </section>
